d50d1b34c06127a4bb5b8d80153d3c053a78db3e
haitang10 <haitang_10@163.com>
2018-11-20 21:25:43 +0800

    完成导航部分

---
+++ src/page/common/nav-simple/index.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: 王贺
+ * @Date:   2018-11-20T11:25:28+08:00
+ * @Last modified by:   王贺
+ * @Last modified time: 2018-11-20T21:09:32+08:00
+ */
+'use strict'
+require('./index.css')
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,25 +2,28 @@
  * @Author: 王贺
  * @Date:   2018-10-22T16:20:16+08:00
  * @Last modified by:   王贺
- * @Last modified time: 2018-11-14T12:00:42+08:00
+ * @Last modified time: 2018-11-20T11:29:21+08:00
  */
 'use strict'
 require('./index.css')
+require('page/common/nav/index.js')
 var _mm = require('util/mm.js')
 
-// 1.测试ajax跨域
-// _mm.request({
-//     url: '/product/list.do?keyword=1',
-//     success: function(res){
-//       console.log(res)
-//     },
-//     error: function(err){
-//         console.log(err)
-//     }
-// })
-// 2.test getUrlParam
-console.log('test getUrlParam', _mm.getUrlParam('test'))
-// 3.test htmlTemplate
-var htmlTemplate = '<div>{{data}}</div>'
-var data = {data: 123}
-console.log('test htmlTemplate', _mm.renderHtml(htmlTemplate, data))
+// // 1.测试ajax跨域
+// // _mm.request({
+// //     url: '/product/list.do?keyword=1',
+// //     success: function(res){
+// //       console.log(res)
+// //     },
+// //     error: function(err){
+// //         console.log(err)
+// //     }
+// // })
+// // 2.test getUrlParam
+// console.log('test getUrlParam', _mm.getUrlParam('test'))
+// // 3.test htmlTemplate
+// var htmlTemplate = '<div>{{data}}</div>'
+// var data = {data: 123}
+// console.log('test htmlTemplate', _mm.renderHtml(htmlTemplate, data))
+
+// 11.20 更新
