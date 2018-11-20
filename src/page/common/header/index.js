5156830626d743c3c71b449b0fdcb817170d582e
haitang10 <haitang_10@163.com>
2018-11-20 23:01:10 +0800

    header.js 函数对象中的this和事件中的this

--- src/page/common/header/index.js
+++ src/page/common/header/index.js
@@ -2,31 +2,35 @@
  * @Author: 王贺
  * @Date:   2018-11-20T21:44:11+08:00
  * @Last modified by:   王贺
- * @Last modified time: 2018-11-20T21:57:14+08:00
+ * @Last modified time: 2018-11-20T23:00:19+08:00
  */
 'use strict'
 require('./index.css')
 var _mm     = require('util/mm.js');
 // 通用页面头部
 var header = {
+
     init : function(){
         this.onLoad()
         this.bindEvent()
     },
     onLoad : function(){
-        var keyword = _mm.getUrlParam('keyword');
+        var keyword = _mm.getUrlParam('keyword')
         // keyword存在，则回填输入框
         if(keyword){
-            $('#search-input').val(keyword);
+            $('#search-input').val(keyword)
         }
     },
     bindEvent : function(){
-        var _this = this;
+        // 事件响应里面的this 是元素,所以要在click事件响应前把this对象赋值出去
+        var _this = this
         // 点击搜索按钮以后，做搜索提交
         $('#search-btn').click(function(){
-            _this.searchSubmit();
+            // console.log('event',event.target)
+            // console.log('this',this)
+            _this.searchSubmit()
         });
-        // 输入会车后，做搜索提交
+        // 输入车后，做搜索提交
         $('#search-input').keyup(function(event){
             // 13是回车键的keyCode
             if(event.keyCode === 13){
@@ -39,12 +43,12 @@ var header = {
         var keyword = $.trim($('#search-input').val())
         // 如果提交的时候有keyword,正常跳转到list页
         if(keyword){
-            window.location.href = './list.html?keyword=' + keyword;
+            window.location.href = './list.html?keyword=' + keyword
         }
         // 如果keyword为空，直接返回首页
         else{
-            _mm.goHome();
+            _mm.goHome()
         }
     }
-};
+}
 header.init()
